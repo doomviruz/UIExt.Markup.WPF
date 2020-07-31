206e3fa0399b960a925fa8c4b7236ed2188f6107
Anton Taranov <ataranoff@gmail.com>
2020-07-31 17:04:21 +0300

    Added ToggleButton extensions. Updated unit-tests.

---
+++ UIExt.Markup.WPF/UIExt.Markup.WPF.Tests/TestToggleButtonExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static UIExt.Markup.WPF.UIExt;
+
+namespace UIExt.Markup.WPF.Tests
+{
+    [TestClass]
+    public class TestToggleButtonExtensions
+    {
+        [TestMethod]
+        public void TestIsChecked()
+        {
+            var control = ToggleButton();
+
+            control.IsChecked(true).IsChecked.Should().Be(true);
+        }
+
+        [TestMethod]
+        public void TestIsThreeState()
+        {
+            var control = ToggleButton();
+
+            control.IsThreeState(true).IsThreeState.Should().Be(true);
+        }
+
+        [TestMethod]
+        public void TestOnChecked()
+        {
+            var control = ToggleButton();
+            var fired = false;
+
+            control.OnChecked((_, e) => fired = true);
+            control.IsChecked = true;
+
+            fired.Should().Be(true);
+        }
+
+        [TestMethod]
+        public void TestOnUnchecked()
+        {
+            var control = ToggleButton();
+            var fired = false;
+
+            control.IsChecked = true;
+            control.OnUnchecked((_, e) => fired = true);
+            control.IsChecked = false;
+
+            fired.Should().Be(true);
+        }
+
+        [TestMethod]
+        public void TestOnIndeterminate()
+        {
+            var control = ToggleButton();
+            var fired = false;
+
+            control.OnIndeterminate((_, e) => fired = true);
+            control.IsChecked = null;
+
+            fired.Should().Be(true);
+        }
+    }
+}
---
+++ UIExt.Markup.WPF/UIExt.Markup.WPF/Extensions/ToggleButtonExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace System.Windows
+{
+    public static class ToggleButtonExtensions
+    {
+        public static T IsChecked<T>(this T target, bool? value) where T : ToggleButton
+        {
+            target.IsChecked = value;
+            return target;
+        }
+
+        public static T IsThreeState<T>(this T target, bool value) where T : ToggleButton
+        {
+            target.IsThreeState = value;
+            return target;
+        }
+
+        public static T OnChecked<T>(this T target, RoutedEventHandler handler) where T : ToggleButton
+        {
+            target.Checked += handler;
+            return target;
+        }
+
+        public static T OnUnchecked<T>(this T target, RoutedEventHandler handler) where T : ToggleButton
+        {
+            target.Unchecked += handler;
+            return target;
+        }
+
+        public static T OnIndeterminate<T>(this T target, RoutedEventHandler handler) where T : ToggleButton
+        {
+            target.Indeterminate += handler;
+            return target;
+        }
+    }
+}
