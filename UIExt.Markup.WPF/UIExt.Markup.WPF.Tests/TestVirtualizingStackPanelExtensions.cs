bb6b0eee48b8790404ab123ef2e3f0c003b52aca
Anton Taranov <ataranoff@gmail.com>
2020-08-01 17:42:39 +0300

    Added VirtualizingStackPanel extensions. Updated unit-tests.

---
+++ UIExt.Markup.WPF/UIExt.Markup.WPF.Tests/TestVirtualizingStackPanelExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static UIExt.Markup.WPF.UIExt;
+
+namespace UIExt.Markup.WPF.Tests
+{
+    [TestClass]
+    public class TestVirtualizingStackPanelExtensions
+    {
+        [TestMethod]
+        public void TestOrientation()
+        {
+            var panel = VirtStackPanel();
+
+            panel.Orientation(Orientation.Horizontal).Orientation.Should().Be(Orientation.Horizontal);
+        }
+
+        [TestMethod]
+        public void TestScrollOwner()
+        {
+            var panel = VirtStackPanel();
+            var scrollViewer = new ScrollViewer();
+
+            panel.ScrollOwner(scrollViewer).ScrollOwner.Should().Be(scrollViewer);
+        }
+    }
+}
