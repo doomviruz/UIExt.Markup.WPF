f20cb316c9995b8dca548245961b1884796436d5
Anton Taranov <ataranoff@gmail.com>
2020-08-01 17:35:17 +0300

    Added StackPanel extensions. Updated unit-tests.

---
+++ UIExt.Markup.WPF/UIExt.Markup.WPF.Tests/TestStackPanelExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static UIExt.Markup.WPF.UIExt;
+
+namespace UIExt.Markup.WPF.Tests
+{
+    [TestClass]
+    public class TestStackPanelExtensions
+    {
+        [TestMethod]
+        public void TestOrientation()
+        {
+            var panel = StackPanel();
+
+            panel.Orientation(Orientation.Horizontal).Orientation.Should().Be(Orientation.Horizontal);
+        }
+
+        [TestMethod]
+        public void TestScrollOwner()
+        {
+            var panel = StackPanel();
+            var scrollViewer = new ScrollViewer();
+
+            panel.ScrollOwner(scrollViewer).ScrollOwner.Should().Be(scrollViewer);
+        }
+    }
+}
---
+++ UIExt.Markup.WPF/UIExt.Markup.WPF/Extensions/StackPanelExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using static UIExt.Markup.WPF.UIExt;
+
+namespace System.Windows
+{
+    public static class StackPanelExtensions
+    {
+        public static T Orientation<T>(this T target, Orientation value) where T : StackPanel
+        {
+            target.Orientation = value;
+            return target;
+        }
+
+        public static T ScrollOwner<T>(this T target, ScrollViewer value) where T : StackPanel
+        {
+            target.ScrollOwner = value;
+            return target;
+        }
+    }
+}
