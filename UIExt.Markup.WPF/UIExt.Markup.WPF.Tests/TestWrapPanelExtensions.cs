f0107568573d0537e9b1979827e9e395683bf475
Anton Taranov <ataranoff@gmail.com>
2020-08-01 18:01:26 +0300

    Added WrapPanel extensions. Updated unit-tests.

---
+++ UIExt.Markup.WPF/UIExt.Markup.WPF.Tests/TestWrapPanelExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static UIExt.Markup.WPF.UIExt;
+
+namespace UIExt.Markup.WPF.Tests
+{
+    [TestClass]
+    public class TestWrapPanelExtensions
+    {
+        [TestMethod]
+        public void TestOrientation()
+        {
+            var panel = WrapPanel();
+
+            panel.Orientation(Orientation.Horizontal).Orientation.Should().Be(Orientation.Horizontal);
+        }
+
+        [TestMethod]
+        public void TestItemWidth()
+        {
+            var panel = WrapPanel();
+
+            panel.ItemWidth(125).ItemWidth.Should().Be(125);
+        }
+
+        [TestMethod]
+        public void TestItemHeight()
+        {
+            var panel = WrapPanel();
+
+            panel.ItemHeight(42).ItemHeight.Should().Be(42);
+        }
+    }
+}
---
+++ UIExt.Markup.WPF/UIExt.Markup.WPF/Extensions/WrapPanelExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+
+namespace System.Windows
+{
+    public static class WrapPanelExtensions
+    {
+        public static T Orientation<T>(this T target, Orientation value) where T : WrapPanel
+        {
+            target.Orientation = value;
+            return target;
+        }
+
+        public static T ItemWidth<T>(this T target, double value) where T : WrapPanel
+        {
+            target.ItemWidth = value;
+            return target;
+        }
+
+        public static T ItemHeight<T>(this T target, double value) where T : WrapPanel
+        {
+            target.ItemHeight = value;
+            return target;
+        }
+    }
+}
