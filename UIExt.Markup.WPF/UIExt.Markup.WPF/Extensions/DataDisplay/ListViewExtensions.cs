0ca2bd5d008238726d25723fdb14dbaf88193400
Anton Taranov <ataranoff@gmail.com>
2020-11-22 10:59:16 +0200

    Added ListView and TreeView extensions.

---
+++ UIExt.Markup.WPF/UIExt.Markup.WPF/Extensions/DataDisplay/ListViewExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace System.Windows
+{
+    public static class ListViewExtensions
+    {
+        public static T RowHeaderTemplate<T>(this T target, ViewBase value) where T : ListView
+        {
+            target.View = value;
+            return target;
+        }
+    }
+}
---
+++ UIExt.Markup.WPF/UIExt.Markup.WPF/Extensions/DataDisplay/TreeViewExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace System.Windows
+{
+    public static class TreeViewExtensions
+    {
+        public static T SelectedValuePath<T>(this T target, string value) where T : TreeView
+        {
+            target.SelectedValuePath = value;
+            return target;
+        }
+    }
+}
